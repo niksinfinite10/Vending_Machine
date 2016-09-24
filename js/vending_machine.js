92014c1f7fc9c4c7e4530c741f0e7903b38af853
niksinfinite10 <nikhil.surati@gmail.com>
2016-09-23 18:02:59 -0700

    make return bug fixes

--- js/vending_machine.js
+++ js/vending_machine.js
@@ -1,4 +1,5 @@
 'use strict'
+const COINENUM = {25:'quarter',10:'dime',5:'nickel'};
 
 const COINS = {nickel:{value:0.05,weight:5.00,diameter:21.21},
             dime:{value:0.10,weight:2.26,diameter:17.91},
@@ -12,9 +13,9 @@ const VALID_COIN ={ nickel:{value:0.05,weight:5.00,diameter:21.21},
                   quarter:{value:0.25,weight:5.67,diameter:24.26}};
 
 const PRODUCT = {
-  cola:{price:1.00,qty:10},
-  chips:{price:0.50,qty:10},
-  candy:{price:0.65,qty:0}
+  cola:{price:1.00,qty:2},
+  chips:{price:0.50,qty:5},
+  candy:{price:0.65,qty:4}
 }
 
 class VendingMachine {
@@ -32,15 +33,20 @@ class VendingMachine {
 
   }
 
-  //FIXME:validate coin more efficiently
+
   isValidCoin(coin){
+    //FIXME:validate coin more efficiently
      return VALID_COIN[coin]?true:false;
    }
 
    acceptCoin(coin){
-     this.insertedCoin[coin] = this.insertedCoin[coin]+1;
+     console.log('beforw insert val',this.loadedCoins);
+     this.loadedCoins[coin] += 1;
+     this.insertedCoin[coin] += 1;
+
      this.updateTotal();
      this.displayMessage = "$ "+this.totalAmount;
+     console.log('insert val',this.loadedCoins);
   }
 
   updateTotal (){
@@ -56,7 +62,7 @@ class VendingMachine {
     //reset the inserted coin stack
     for(let val in this.insertedCoin)
         this.insertedCoin[val]=0;
-    //reset the total amount
+
     this.totalAmount = 0;
   }
 
@@ -66,17 +72,15 @@ class VendingMachine {
           //Product is available checking the price of the item
           if(this.totalAmount >= PRODUCT[itemName].price){
             remainingChange = (parseFloat(this.totalAmount)-parseFloat(PRODUCT[itemName].price)).toPrecision(3);
-              console.log('reain',remainingChange);
+              // console.log('reain',remainingChange);
+              //deduct the quantity from the inventory
+              PRODUCT[itemName].qty = PRODUCT[itemName].qty-1;
 
-              //FIXME:fix compute change function with real value
-              let changeArr = this.computeChange([25,10, 5],remainingChange*100);
-              // let changeArr = this.computeChange([0.25, 0.10, 0.05],0);
-                  this.change = changeArr[0]>0?'0.25 x '+changeArr[0]:'';
-                  this.change +=changeArr[1]>0?' 0.10 x '+changeArr[1]:'';
-                  this.change +=changeArr[2]>0?' 0.05 x '+changeArr[2]:'';
+              this.makeReturnChange(remainingChange);
 
-              //reset the values
+
               let returnChange = this.change;
+              //reset the values
               this.reset();
               this.displayMessage = 'THANK YOU';
               return ['THANK YOU','0.00',returnChange];
@@ -96,15 +100,35 @@ class VendingMachine {
       }
   }
 
-//FIXME: return the change with the coin name
+  makeReturnChange(amount){
+
+    //TODO: fix coin stack with the current coin stack
+    let coinStack = [10, 5];
+    let changeArr = this.computeChange(coinStack,amount*100);
+    for(let i=0;i<changeArr.length;i++){
+      this.loadedCoins[COINENUM[coinStack[i]]] = this.loadedCoins[COINENUM[coinStack[i]]]-changeArr[i];
+      if(i==0)
+        this.change = changeArr[i]>0?COINENUM[coinStack[i]]+''+changeArr[i]:'';
+      else
+        this.change += changeArr[i]>0?COINENUM[coinStack[i]]+''+changeArr[i]:'';
+    }
+
+    //substract the quantity of change coins from the loadedCoins
+    // this.loadedCoins['quarter'] = this.loadedCoins['quarter']-changeArr[0];
+    // this.loadedCoins['dime'] = this.loadedCoins['dime']- changeArr[1];
+    // this.loadedCoins['nickel'] = this.loadedCoins['nickel']-changeArr[2];
+
+    // this.change = changeArr[0]>0?'0.10 x '+changeArr[0]:'';
+    // this.change +=changeArr[1]>0?' 0.5 x '+changeArr[1]:'';
+    // this.change +=changeArr[2]>0?' 0.05 x '+changeArr[2]:'';
+    return this.change;
+
+  }
+
   returnCoin(){
       if(this.totalAmount>0){
         this.change = this.totalAmount;
-        let changeArr = this.computeChange([25,10, 5],this.change*100);
-        this.change = changeArr[0]>0?'0.25 x '+changeArr[0]:'';
-        this.change +=changeArr[1]>0?' 0.10 x '+changeArr[1]:'';
-        this.change +=changeArr[2]>0?' 0.05 x '+changeArr[2]:'';
-
+        this.makeReturnChange(this.change);
         this.displayMessage = 'INSERT COIN';
         this.reset();
       }
